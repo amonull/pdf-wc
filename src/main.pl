1124b299f40dcb5bd6ef4226490ee577b0db98ef
amonull <90932550+amonull@users.noreply.github.com>
2023-05-01 15:39:33 +0100

    feature: ignore reading pages

--- src/main.pl
+++ src/main.pl
@@ -8,11 +8,18 @@ use CAM::PDF::PageText;
 
 my $pdf = CAM::PDF->new("/home/amon/Documents/lectures/oop/main-assignment-report/report-ver0_1.pdf");
 my $total_pdf_pages = $pdf->numPages;
+my $break_point = ' '; # for reading words
+# my $break_point = '\n'; # for reading lines
+my @ignore_pages = qw(1 2);
 
 sub count_all_pages() {
   my $return_count = 0;
   foreach my $num (1..$total_pdf_pages) {
-    my @read_page = split(' ', CAM::PDF::PageText->render($pdf->getPageContentTree($num)));
+    # TODO: remove grep with a more efficinet/faster way to check if item is in @ignore_pages
+    if (@ignore_pages && grep /^$num$/, @ignore_pages) {
+      next;
+    }
+    my @read_page = split $break_point, CAM::PDF::PageText->render($pdf->getPageContentTree($num));
     $return_count += scalar @read_page;
   }
 
