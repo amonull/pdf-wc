ee78b40fcdd8db430e36944dfdc2432350a41aa7
amonull <90932550+amonull@users.noreply.github.com>
2023-05-03 11:30:59 +0100

    adding flags

--- src/main.pl
+++ src/main.pl
@@ -3,16 +3,44 @@
 use warnings;
 use strict;
 
+use Getopt::Long qw(GetOptions);
+
 use CAM::PDF;
 use CAM::PDF::PageText;
 
-my $pdf = CAM::PDF->new("/home/amon/Documents/lectures/oop/main-assignment-report/report-ver0_1.pdf");
-my $total_pdf_pages = $pdf->numPages;
-my $break_point = ' '; # for reading words
-# my $break_point = '\n'; # for reading lines
-my @ignore_pages = qw(1 2);
+my @ignore_pages = ();
+my $count_lines = 0;
+my $print_help = 0;
+GetOptions( "ignore-pages|i=i{,}" => \@ignore_pages,
+            "count-lines|l" => \$count_lines,
+            "help|h" => print_usage() ) or die print_usage();
 
-sub count_all_pages() {
+my $break_point = $count_lines ? '\n' : ' ';
+
+sub print_usage {
+  print "Usage: \[option\] \[file name(s)\]" . "\n";
+  print "\n";
+  print "Options:\n";
+  print "\t" . "-i, --ignore-pages <num(s)>" . "\n";
+  print "\t\t" . "pages to ignore" . "\n";
+  print "\t" . "-l, --count-lines" . "\n";
+  print "\t\t" . "count lines instead of words" . "\n";
+  print "\t" . "-h, --help" . "\n";
+  print "\t\t" . "shows this" . "\n";
+ 
+  exit;
+}
+
+sub count_all_pages($) {
+
+  my $pdf = CAM::PDF->new($_[0]);
+
+  if (!$pdf) {
+    return -1;
+  }
+
+  my $total_pdf_pages = $pdf->numPages;
+
   my $return_count = 0;
   foreach my $num (1..$total_pdf_pages) {
     # TODO: remove grep with a more efficinet/faster way to check if item is in @ignore_pages
@@ -26,6 +54,17 @@ sub count_all_pages() {
   return $return_count;
 }
 
-my $word_count_on_all = count_all_pages();
+foreach my $pdf_path (@ARGV) {
+  if ( !(-e $pdf_path) ) {
+    print "File does not exists skipping $pdf_path \n";
+    next;
+  }
 
-print $word_count_on_all . "\n";
+  my $result = count_all_pages($pdf_path);
+  if ($result lt 0) {
+    print "$pdf_path could not be read. File most likely not a pdf file. skipping";
+    next;
+  }
+
+  print $result . "\n";
+}
